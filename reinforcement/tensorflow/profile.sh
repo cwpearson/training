3005387983b1e53165d0538c828913a69fdd32e8
Carl Pearson <Carl.Pearson@ibm.com>
2018-10-21 16:48:07 -0400

    add a bit more info to profile.sh

--- reinforcement/tensorflow/profile.sh
+++ reinforcement/tensorflow/profile.sh
@@ -3,6 +3,15 @@
 # Run minigo... stop when it converges.
 set -e
 
+echo "==== hostname ===="
+hostname
+
+echo "==== lscpu ===="
+lscpu
+
+echo "==== nvidia-smi ===="
+nvidia-smi
+
 SEED=$1
 mkdir -p /research/results/minigo/final/
 cd /research/reinforcement/minigo
