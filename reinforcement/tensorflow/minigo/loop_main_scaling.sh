a1520feadae76c728ad3bd9a34c4b07913819d9e
Carl Pearson <carl.pearson@us.ibm.comwq>
2018-10-23 10:55:22 -0400

    add scripts for runs to check performance scaling

---
+++ reinforcement/tensorflow/minigo/loop_main_scaling.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# We achieve parallelism through multiprocessing for minigo.
+# This technique is rather crude, but gives the necessary
+# speedup to run the benchmark in a useful length of time.
+
+set -e
+
+SEED=$2
+
+FILE="TERMINATE_FLAG"
+rm -f $FILE
+
+
+echo "====BEGIN INIT===="
+GOPARAMS=$1 python3 loop_init.py
+for i in {1..3};
+do
+echo "====BEGIN SELFPLAY====";
+date;
+GOPARAMS=$1 python3 loop_selfplay.py $SEED $i 2>&1
+date;
+echo "====END SELFPLAY====";
+
+echo "====BEGIN TRAIN_EVAL====";
+date;
+GOPARAMS=$1 python3 loop_train_eval.py $SEED $i 2>&1
+date;
+echo "====END TRAIN_EVAL====";
+
+
+
+if [ -f $FILE ]; then
+   echo "$FILE exists: finished!"
+   cat $FILE
+   break
+else
+   echo "$FILE does not exist; looping again."
+fi
+done
