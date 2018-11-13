8d2486598f4ba54127ae2c34c1d06531f4c3a188
Carl Pearson <Carl.Pearson@ibm.com>
2018-11-13 11:07:11 -0500

    add cat of params to scaling.sh

--- reinforcement/tensorflow/scaling.sh
+++ reinforcement/tensorflow/scaling.sh
@@ -3,6 +3,8 @@
 # Run minigo... stop when it converges.
 set -e
 
+PARAMS_FILE=params/scaling.json
+
 echo "==== hostname ===="
 hostname
 
@@ -12,7 +14,12 @@ lscpu
 echo "==== nvidia-smi ===="
 nvidia-smi
 
+echo "==== params ===="
+echo "reading from minigo/$PARAMS_FILE"
+cat minigo/$PARAMS_FILE
+
 SEED=$1
 mkdir -p /research/results/minigo/final/
 cd /research/reinforcement/minigo
 bash loop_main_scaling.sh params/scaling.json $SEED
+
